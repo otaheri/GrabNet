73ac9fb8a4bc20dca21bcf1cdcdc519e68408bde
Omid Taheri <omid.taheri.71@gmail.com>
2020-08-30 22:40:45 +0200

    use new added tester instead of trainer

--- grabnet/tests/grab_new_objects.py
+++ grabnet/tests/grab_new_objects.py
@@ -20,13 +20,73 @@ import argparse
 
 import mano
 from psbody.mesh import MeshViewers, Mesh
-from grabnet.tools.vis_tools import vis_results
-
+from grabnet.tools.vis_tools import points_to_spheres
 from grabnet.tools.utils import euler
 from grabnet.tools.cfg_parser import Config
-from grabnet.train.trainer import Trainer
+from grabnet.tests.tester import Tester
 
 from bps_torch.bps import bps_torch
+
+from psbody.mesh.colors import name_to_rgb
+from grabnet.tools.train_tools import point2point_signed
+from grabnet.tools.utils import aa2rotmat
+from grabnet.tools.utils import makepath
+from grabnet.tools.utils import to_cpu
+
+
+def vis_results(dorig, coarse_net, refine_net, rh_model , save=False, save_dir = None):
+
+    with torch.no_grad():
+        imw, imh = 1920, 780
+        cols = len(dorig['bps_object'])
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        mvs = MeshViewers(window_width=imw, window_height=imh, shape=[1, cols], keepalive=True)
+
+        drec_cnet = coarse_net.sample_poses(dorig['bps_object'])
+        verts_rh_gen_cnet = rh_model(**drec_cnet).vertices
+
+        _, h2o, _ = point2point_signed(verts_rh_gen_cnet, dorig['verts_object'].to(device))
+
+        drec_cnet['trans_rhand_f'] = drec_cnet['transl']
+        drec_cnet['global_orient_rhand_rotmat_f'] = aa2rotmat(drec_cnet['global_orient']).view(-1, 3, 3)
+        drec_cnet['fpose_rhand_rotmat_f'] = aa2rotmat(drec_cnet['hand_pose']).view(-1, 15, 3, 3)
+        drec_cnet['verts_object'] = dorig['verts_object'].to(device)
+        drec_cnet['h2o_dist']= h2o.abs()
+
+        drec_rnet = refine_net(**drec_cnet)
+        verts_rh_gen_rnet = rh_model(**drec_rnet).vertices
+
+
+        for cId in range(0, len(dorig['bps_object'])):
+            try:
+                from copy import deepcopy
+                meshes = deepcopy(dorig['mesh_object'])
+                obj_mesh = meshes[cId]
+            except:
+                obj_mesh = points_to_spheres(to_cpu(dorig['verts_object'][cId]), radius=0.002, vc=name_to_rgb['green'])
+
+            hand_mesh_gen_cnet = Mesh(v=to_cpu(verts_rh_gen_cnet[cId]), f=rh_model.faces, vc=name_to_rgb['pink'])
+            hand_mesh_gen_rnet = Mesh(v=to_cpu(verts_rh_gen_rnet[cId]), f=rh_model.faces, vc=name_to_rgb['gray'])
+
+            if 'rotmat' in dorig:
+                rotmat = dorig['rotmat'][cId].T
+                obj_mesh = obj_mesh.rotate_vertices(rotmat)
+                hand_mesh_gen_cnet.rotate_vertices(rotmat)
+                hand_mesh_gen_rnet.rotate_vertices(rotmat)
+
+            hand_mesh_gen_cnet.reset_face_normals()
+            hand_mesh_gen_rnet.reset_face_normals()
+
+            # mvs[0][cId].set_static_meshes([hand_mesh_gen_cnet] + obj_mesh, blocking=True)
+            mvs[0][cId].set_static_meshes([hand_mesh_gen_rnet,obj_mesh], blocking=True)
+
+            if save:
+                save_path = os.path.join(save_dir, str(cId))
+                makepath(save_path)
+                hand_mesh_gen_rnet.write_ply(filename=save_path + '/rh_mesh_gen_%d.ply' % cId)
+                obj_mesh[0].write_ply(filename=save_path + '/obj_mesh_%d.ply' % cId)
+
 
 def grab_new_objs(grabnet, objs_path, rot=True, n_samples=10, scale=1.):
     
@@ -44,8 +104,7 @@ def grab_new_objs(grabnet, objs_path, rot=True, n_samples=10, scale=1.):
     grabnet.logger(f'################# \n'
                    f'Colors Guide:'
                    f'                   \n'
-                   f'Pink  --->  CoarseNet generated grasp\n'
-                   f'Gray  --->  RefineNet generated grasp\n')
+                   f'Gray  --->  GrabNet generated grasp\n')
 
     bps = bps_torch(custom_basis = grabnet.bps)
 
@@ -87,13 +146,11 @@ def grab_new_objs(grabnet, objs_path, rot=True, n_samples=10, scale=1.):
                     coarse_net=grabnet.coarse_net,
                     refine_net=grabnet.refine_net,
                     rh_model=rh_model,
-                    show_rec=False,
-                    show_gen=True,
                     save=False,
                     save_dir=save_dir
                     )
 
-def load_obj_verts(mesh_path, rand_rotmat, rndrotate=True, scale=1000., n_sample_verts=5000):
+def load_obj_verts(mesh_path, rand_rotmat, rndrotate=True, scale=1., n_sample_verts=10000):
 
     np.random.seed(100)
     obj_mesh = Mesh(filename=mesh_path, vscale=scale)
@@ -103,7 +160,7 @@ def load_obj_verts(mesh_path, rand_rotmat, rndrotate=True, scale=1000., n_sample
 
     ## center and scale the object
     max_length = np.linalg.norm(obj_mesh.v, axis=1).max()
-    if  max_length > .15:
+    if  max_length > .3:
         re_scale = max_length/.08
         print(f'The object is very large, down-scaling by {re_scale} factor')
         obj_mesh.v = obj_mesh.v/re_scale
@@ -132,13 +189,10 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='GrabNet-Testing')
 
-    parser.add_argument('--data-path', default=None, type=str,
-                        help='The path to the folder that contains GrabNet data')
-
     parser.add_argument('--obj-path', required = True, type=str,
                         help='The path to the 3D object Mesh or Pointcloud')
 
-    parser.add_argument('--rhm-path', default = None, type=str,
+    parser.add_argument('--rhm-path', required = True, type=str,
                         help='The path to the folder containing MANO_RIHGT model')
 
     parser.add_argument('--config-path', default= None, type=str,
@@ -147,17 +201,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     cfg_path = args.config_path
-    data_path = args.data_path
     obj_path = args.obj_path
     rhm_path = args.rhm_path
 
     cwd = os.getcwd()
-    work_dir = cwd + '/test'
+    work_dir = cwd + '/logs'
 
     best_cnet = 'grabnet/models/coarsenet.pt'
     best_rnet = 'grabnet/models/refinenet.pt'
-    vpe_path = 'grabnet/configs/verts_per_edge.npy'
-    c_weights_path = 'grabnet/configs/rhand_weight.npy'
+    bps_dir   = 'grabnet/configs/bps.npz'
+
 
     if cfg_path is None:
         cfg_path = 'grabnet/configs/grabnet_cfg.yaml'
@@ -165,21 +218,13 @@ if __name__ == '__main__':
 
     config = {
         'work_dir': work_dir,
-        'vpe_path': vpe_path,
-        'c_weights_path': c_weights_path,
+        'best_cnet': best_cnet,
+        'best_rnet': best_rnet,
+        'bps_dir': bps_dir,
+        'rhm_path': rhm_path
     }
 
     cfg = Config(default_cfg_path=cfg_path, **config)
 
-    if data_path is not None:
-        cfg['dataset_dir'] = data_path
-
-    if rhm_path is not None:
-        cfg['rhm_path'] = rhm_path
-    if cfg.best_cnet is  None:
-        cfg['best_cnet'] = best_cnet
-    if cfg.best_rnet is None:
-        cfg['best_rnet'] = best_rnet
-
-    grabnet = Trainer(cfg=cfg, inference=True)
-    grab_new_objs(grabnet,obj_path)
+    grabnet = Tester(cfg=cfg)
+    grab_new_objs(grabnet,obj_path, rot=True, n_samples=10)
