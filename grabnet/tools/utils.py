9c10b70a0d9ddb0a892e1d377297f34ec2a0f192
Omid Taheri <omid.taheri.71@gmail.com>
2020-08-30 19:38:20 +0200

    remove extra toold

--- grabnet/tools/utils.py
+++ grabnet/tools/utils.py
@@ -80,51 +80,6 @@ def makelogger(log_dir,mode='w'):
 
     return logger
 
-
-class DotDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(DotDict, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.items():
-                    if isinstance(v, dict):
-                        v = DotDict(v)
-                    if isinstance(v, list):
-                        self.__convert(v)
-                    self[k] = v
-
-        if kwargs:
-            for k, v in kwargs.items():
-                if isinstance(v, dict):
-                    v = DotDict(v)
-                elif isinstance(v, list):
-                    self.__convert(v)
-                self[k] = v
-
-    def __convert(self, v):
-        for elem in range(0, len(v)):
-            if isinstance(v[elem], dict):
-                v[elem] = DotDict(v[elem])
-            elif isinstance(v[elem], list):
-                self.__convert(v[elem])
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(DotDict, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-def convert_dict2ntuple(in_dict):
-    out_dict = copy(in_dict)
-    for k,v in out_dict.items():
-       if isinstance(v,dict):
-           out_dict[k] = convert_dict2ntuple(v)
-    return namedtuple('GDict', out_dict.keys())(**out_dict)
-
 def CRot2rotmat(pose):
 
     reshaped_input = pose.view(-1, 3, 2)
