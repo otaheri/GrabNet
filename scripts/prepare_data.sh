c90ce891952e251bed5fd37bf054d2272bad59ee
Omid Taheri <omid.taheri@tuebingen.mpg.de>
2024-03-28 13:23:09 +0100

    Update new data on gdrive

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -3,7 +3,8 @@
 mkdir -p data
 cd data
 #gdown "https://drive.google.com/file/d/1L_We51j5FwIo5PeLvAK_9I3ZuFU_IWAq/view?usp=sharing"
-gdown "https://drive.google.com/uc?id=1L_We51j5FwIo5PeLvAK_9I3ZuFU_IWAq"
+#gdown "https://drive.google.com/uc?id=1L_We51j5FwIo5PeLvAK_9I3ZuFU_IWAq"
+gdown "https://drive.google.com/uc?id=1OQdyZv05ohWQ2bl3sZ09jwAaY6HbQtfQ"
 unzip grabnet_data.zip
 rm grabnet_data.zip
 cd ..
